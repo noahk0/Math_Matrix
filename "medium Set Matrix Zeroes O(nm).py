3c0b81e63cdcf6fc13a36f65004a0da463e2c12e
noahk0 <noah.kim@mail.utoronto.ca>
2024-08-24 14:58:19 +0900

    Create "medium Set Matrix Zeroes O(nm).py

---
+++ "medium Set Matrix Zeroes O(nm).py
@@ -0,0 +1,13 @@
+def setZeroes(self, matrix: List[List[int]]) -> None:
+    row, col = False, False
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            if not matrix[i][j]:
+                row.add(i)
+                col.add(j)
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            if i in row or j in col:
+                matrix[i][j] = 0
